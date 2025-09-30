45910480927240ce0e90d382082bf2791ff5719b
clara <claraaalvarez23@gmail.com>
2025-09-30 16:02:54 +0200

    ajustes2 main

--- Main.java
+++ Main.java
@@ -1,31 +1,36 @@
 public class Main {
-    public static void Main(String[]args) {
-        System.out.println("Creando los prototipos");
+    public static void main(String[] args) {
+        Registro registro = new Registro();
+
+        System.out.println("Creando prototipos...");
         long start = System.currentTimeMillis();
-        registry.addPrototype("fibra", new Fibra());
-        registry.addPrototype("movil", new Movil());
-        registry.addPrototype("fusion", new ClienteFusion());
-        registry.addPrototype("nuevo", new NuevoCliente());
+
+        registro.addPrototipo("fibra", new Fibra());
+        registro.addPrototipo("movil", new Movil());
+        registro.addPrototipo("fusion", new ClienteFusion());
+        registro.addPrototipo("nuevo", new NuevoCliente());
 
         long end = System.currentTimeMillis();
-        System.out.println("Tiempo en crear prototipos:" + (end - start) + "ms/n");
+        System.out.println("Tiempo en crear prototipos: " + (end - start) + "ms\n");
 
-        System.out.println("Clonando prototipos");
-
+        System.out.println("Clonando prototipos...");
         start = System.currentTimeMillis();
-        Prototype cliente1 = registro.getClone("fibra");
+
+        Prototype cliente1 = registro.getPrototipo("fibra");
         cliente1.username = "Carla Lazon";
         cliente1.offers.add("GB ilimitados");
 
-        Prototype cliente2 = registro.getClone("fusion");
-        cliente1.username = "Clara Alvarez";
-        cliente1.offers.add("Promo futbol para estudiantes");
+        Prototype cliente2 = registro.getPrototipo("fusion");
+        cliente2.username = "Clara Alvarez";
+        cliente2.offers.add("Promo futbol para estudiantes");
+
         end = System.currentTimeMillis();
+        System.out.println("Tiempo en clonar: " + (end - start) + "ms\n");
 
-        System.out.println(("Tiempo en clonar:" + (end - start) + "ms/n"););
+        System.out.println("Clienta Carla (clon de Fibra): ");
+        cliente1.mostrarInfo();
 
-        System.out.println("Clienta Carla (clon de Fibra): \n"+ cliente1);
-        System.out.println("Clienta Clara (clon de Fusion): \n"+ cliente2);
-
+        System.out.println("Clienta Clara (clon de Fusion): ");
+        cliente2.mostrarInfo();
     }
 }
