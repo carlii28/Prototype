b4ab2f4f5b2d145576409a095fbc53c31bd60aec
clara <claraaalvarez23@gmail.com>
2025-09-30 15:54:12 +0200

    ajustes main

--- Main.java
+++ Main.java
@@ -4,7 +4,28 @@ public class Main {
         long start = System.currentTimeMillis();
         registry.addPrototype("fibra", new Fibra());
         registry.addPrototype("movil", new Movil());
-        registry.addPrototype("fusion", new
+        registry.addPrototype("fusion", new ClienteFusion());
+        registry.addPrototype("nuevo", new NuevoCliente());
+
+        long end = System.currentTimeMillis();
+        System.out.println("Tiempo en crear prototipos:" + (end - start) + "ms/n");
+
+        System.out.println("Clonando prototipos");
+
+        start = System.currentTimeMillis();
+        Prototype cliente1 = registro.getClone("fibra");
+        cliente1.username = "Carla Lazon";
+        cliente1.offers.add("GB ilimitados");
+
+        Prototype cliente2 = registro.getClone("fusion");
+        cliente1.username = "Clara Alvarez";
+        cliente1.offers.add("Promo futbol para estudiantes");
+        end = System.currentTimeMillis();
+
+        System.out.println(("Tiempo en clonar:" + (end - start) + "ms/n"););
+
+        System.out.println("Clienta Carla (clon de Fibra): \n"+ cliente1);
+        System.out.println("Clienta Clara (clon de Fusion): \n"+ cliente2);
 
     }
 }
